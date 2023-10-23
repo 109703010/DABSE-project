c0b22ee8ee1a801109bc687b04088413856b65b4
owen0621 <owen10270936@gmail.com>
2023-10-24 00:35:00 +0800

    feat: find generators

--- setup.c
+++ setup.c
@@ -11,6 +11,13 @@ typedef struct order {
     mpz_t N;
 } ORDER;
 
+typedef struct generator {
+    element_t g;
+    element_t g_1;
+    element_t g_2;
+    element_t g_3;
+} GEN;
+
 void setup() {
     ORDER group_order;
     mpz_init_set_ui(group_order.p_1, 941083987);
@@ -24,15 +31,13 @@ void setup() {
     gmp_printf("p3: %Zd \n", group_order.p_3);
     gmp_printf("N: %Zd \n", group_order.N);
 #endif
-
-    // pairing_t custom_pairing;
-    // pbc_param_t custom_param;
-    // element_t generator;
-
-    // pbc_param_init_a1_gen(custom_param, N);  // 使用 N 作为阶
-    // pairing_init_pbc_param(custom_pairing, custom_param);
-
-    // element_init_G1(generator, custom_pairing);  // 创建 G1 群的生成元
+    pairing_t custom_pairing;
+    pbc_param_t custom_param;
+    element_t g, g_1;
+    pbc_param_init_a1_gen(custom_param, group_order.N);  // 使用 N 作为阶
+    pairing_init_pbc_param(custom_pairing, custom_param);
+    element_init_G1(g, custom_pairing);  // 创建 G1 群的生成元
+    element_random(g);                   // 随机选择一个生成元 g
 }
 
 int main() {
