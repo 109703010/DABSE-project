514835b7f8463b87df4ce3c2fdf8cc7f1967c07c
owen0621 <owen10270936@gmail.com>
2023-10-24 17:56:24 +0800

    feat: find generators

--- setup.c
+++ setup.c
@@ -16,13 +16,14 @@ typedef struct generator {
     element_t g_1;
     element_t g_2;
     element_t g_3;
-} GEN;
+} GENERATOR;
 
 void setup() {
     ORDER group_order;
     mpz_init_set_ui(group_order.p_1, 941083987);
     mpz_init_set_ui(group_order.p_2, 573259433);
     mpz_init_set_ui(group_order.p_3, 334214467);
+    mpz_init(group_order.N);
     mpz_mul(group_order.N, group_order.p_1, group_order.p_2);
     mpz_mul(group_order.N, group_order.N, group_order.p_3);
 #if defined(DEBUG)
@@ -33,11 +34,32 @@ void setup() {
 #endif
     pairing_t custom_pairing;
     pbc_param_t custom_param;
-    element_t g, g_1;
-    pbc_param_init_a1_gen(custom_param, group_order.N);  // 使用 N 作为阶
+    GENERATOR gen;
+    element_t check;
+    mpz_t tmp;
+    mpz_init(tmp);
+    pbc_param_init_a1_gen(custom_param, group_order.N);  // 使用N作为order
     pairing_init_pbc_param(custom_pairing, custom_param);
-    element_init_G1(g, custom_pairing);  // 创建 G1 群的生成元
-    element_random(g);                   // 随机选择一个生成元 g
+    element_init_G1(gen.g, custom_pairing);    // 创建G的生成元
+    element_init_G1(gen.g_1, custom_pairing);  // 创建G的生成元
+    element_init_G1(gen.g_2, custom_pairing);  // 创建G的生成元
+    element_init_G1(gen.g_3, custom_pairing);  // 创建G的生成元
+    element_init_G1(check, custom_pairing);
+    do {
+        element_random(gen.g_1);  // 随机选择一个生成元
+        mpz_mul(tmp, group_order.p_2, group_order.p_3);
+        element_pow_mpz(check, gen.g_1, tmp);  // 计算 g^(N/p1)
+    } while (element_is1(check));              // g^(N/p1)==identity
+    do {
+        element_random(gen.g_2);  // 随机选择一个生成元
+        mpz_mul(tmp, group_order.p_1, group_order.p_3);
+        element_pow_mpz(check, gen.g_2, tmp);  // 计算 g^(N/p2)
+    } while (element_is1(check));              // g^(N/p1)==identity
+    do {
+        element_random(gen.g_3);  // 随机选择一个生成元
+        mpz_mul(tmp, group_order.p_1, group_order.p_2);
+        element_pow_mpz(check, gen.g_3, tmp);  // 计算 g^(N/p3)
+    } while (element_is1(check));              // g^(N/p1)==identity
 }
 
 int main() {
