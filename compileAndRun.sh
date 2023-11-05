fd871f0cc31d1ee37ec69fbfdb60c1e7db96b374
Andy <andysztzn@gmail.com>
2023-11-05 21:15:03 +0800

    Initial Commit
    Note:
    - This branch should never be merged into main
    - This branch should be up-to-date as the main branch being updated
    
    Function in progress:
    - trapgen
    - test
    - test2
    - dec

---
+++ compileAndRun.sh
@@ -0,0 +1,2 @@
+gcc -o a.out main.c policy/obj/data_structure/*/* -lgmp -lpbc -lssl -lcrypto
+./a.out
---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <gmp.h>
+#include "policy/createPolicy.h"
+#include "setup.h"
+#include "enc.h"
+#include "trapgen.h"
+#include "test.h"
+#include "test2.h"
+#include "dec.h"
+
+
+int main(void) {
+  /*** Initialization Begin ***/
+
+  // For setup
+  mpz_t lambda;
+  mpz_init_set_ui(lambda, 256);
+  
+  // For enc
+  char* message = "Long long way to go...";
+  char* attributeName[] = {"Hospital", "Department", "Sex", "Doctor number"};
+  char* accessString = "((A & D) | ((A & B) & C))"; 
+  Policy A = convertToPolicy(accessString);
+  char* T[] = {"Central hospital", "311-4321", "City hospital", "Genetics", "Male"};
+  
+  // For trapgen
+  char* testAttributeName[2][3] = {{"Hospital", "Sex", "Doctor number"}, {"Hospital", "Sex", "Department"}};
+  char* LS[2][3] = {{"Central hospital", "Male", "311-4321"}, {"City hospital", "Female", "Genetics"}};
+
+  /*** Initialization End ***/
+
+  
+  /*** Execution Begin***/
+
+  setup(lambda);
+  Enc(epsilon, message, A, T);
+  trapgen(epsilon);
+  test(epsilon, C, ts);
+  test2(epsilon);
+  dec();
+
+  /*** Execution End***/
+  return 0;
+}
